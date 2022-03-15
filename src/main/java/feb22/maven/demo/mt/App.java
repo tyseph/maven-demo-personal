52cdb7e4fae56435cf60405943bd7adf77286345
Sajal Ganjewala <sg748@snu.edu.in>
2022-03-15 12:29:20 +0530

    Concurrency

--- src/main/java/feb22/maven/demo/mt/App.java
+++ src/main/java/feb22/maven/demo/mt/App.java
@@ -2,14 +2,18 @@ package feb22.maven.demo.mt;
 
 // Multi-threading with Thread class
 
-public class App extends Thread {
-
+//public class App extends Thread {
+public class App implements Runnable {
 	void printNums() {
 
-		for (int i = 0; i <= 10; i++) {
-			System.out.println(i);
+//		Thread.currentThread().setName("ABC");
+		Thread.currentThread().setPriority(9);
+		System.out.println(Thread.currentThread().getPriority());
+
+		for (int i = 0; i <= 3; i++) {
+			System.out.println(i + " - " + Thread.currentThread().getName());
 			try {
-				Thread.sleep(500);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -23,13 +27,24 @@ public class App extends Thread {
 
 	public static void main(String[] args) {
 
-		App app = new App();
+//		App app = new App();
+//		app.start();
+//		App app2 = new App();
+//		app2.start();
+
+		Thread app = new Thread(new App());
+		Thread app2 = new Thread(new App());
+		Thread app3 = new Thread(new App());
+
 		app.start();
-		App app2 = new App();
 		app2.start();
-		App app3 = new App();
+		try {
+			app2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		app3.start();
-		App app4 = new App();
-		app4.start();
+
 	}
 }
